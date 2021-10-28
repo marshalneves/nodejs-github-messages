8589ff21ccf9b3fd8986d617f496aa29e1517ced
Marshal Neves <mcneves75@gmail.com>
2021-10-28 18:24:41 -0300

    Implementing Message Creation and WebSocket

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
 import express, { response } from 'express';
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log('User connected on socket');
+})
+
 app.use(express.json());
 
 app.use(router);
@@ -17,4 +33,4 @@ app.get("/signin/callback", (request, response) => {
     return response.json(code);
 })
 
-app.listen(4000, () => console.log('Server is running on PORT 4000'));
+export { serverHttp, io }
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
+import { Router } from "express";
+
 import { AuthenticateUserController } from "./controllers/AutenticateUserController";
-import {Router} from "express";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
 
 // work as a middleware
 router.post("/authenticate", new AuthenticateUserController().handle);
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
 
 export { router }
