06d8c30b1d9c6afe8d8ee5b5cf3f776ee1c38322
Marshal Neves <mcneves75@gmail.com>
2021-10-28 18:51:32 -0300

    Implementing profile user route

---
+++ src/controllers/Get3LastMessagesController.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from "express";
+import { GetLast3MessagesService } from "../services/Get3LastMessagesService";
+
+
+class Get3LastMessagesController {
+    async handle(request: Request, response: Response) {
+        const service = new GetLast3MessagesService();
+
+        const result = await service.execute();
+
+        return response.json(result);
+
+    }
+}
+
+export { Get3LastMessagesController }
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import { AuthenticateUserController } from "./controllers/AutenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/Get3LastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
@@ -9,5 +11,7 @@ const router = Router();
 // work as a middleware
 router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+router.get("/messages/last3", new Get3LastMessagesController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router }
